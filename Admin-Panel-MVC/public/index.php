3328462b89fce200c476c795ae93cdd8917808de
Srs Mayur <srs.mayur@gmail.com>
2018-02-28 18:24:17 +0530

    Admin-Panel-MVC

---
+++ Admin-Panel-MVC/public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+error_reporting(E_ALL);
+set_error_handler('Core\Error::errorHandler');
+set_exception_handler('Core\Error::exceptionHandler');
+
+$router = new Core\Router();
+
+$router->add('',['controller' => 'Home','action' => 'index']);
+$router->add('{controller}/{action}');
+
+$router->dispatch($_SERVER['QUERY_STRING']);
+
+?>
